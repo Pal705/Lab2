546a244434dee3082f27c87035124820416ba6b0
pal705 <paulchampagne18@gmail.com>
2014-10-06 13:54:08 -0500

    Ajouter les constantes et les variables dans la classe Taxi. Création du constructeur.

--- Lab2_matricule/src/client/Taxi.java
+++ Lab2_matricule/src/client/Taxi.java
@@ -1,5 +1,81 @@
 package client;
 
+//Paul Champagne
+//Jean-Luc Manseau
+
+/**
+ * 
+ * @author Paul Champagne
+ * @version 1.0.0
+ */
+
 public class Taxi {
 
+	//Déclaration des constantes
+	
+	double CONSOMMATION = 8.45;
+	double COUT_PAR_LITRE = 2.37;
+	double REVENU_PAR_KILOMETRE_AFFAIRE = 3;
+	double REVENU_PAR_KILOMETRE_LOCAL = 3.5;
+	double REVENU_PAR_KILOMETRE_TOURISTIQUE = 5;
+	double REVENU_PAR_KILOMETRE_PROMO = 2;
+	double REVENU_PAR_KILOMETRE_LUXE = 9.5;
+
+	//Déclaration des variables
+	
+	String nom;
+	double montantGagne;
+	double distanceParcourueTotale;
+	double distanceDepuisDepense;
+	int anneeCourante;
+	int moisCourant;
+	int jourCourant;
+	
+	
+	
+	
+	
+	public Taxi(String p_nom) {
+		
+		nom = p_nom;
+		
+				
+	}
+
+
+
+
+
+	public double getMontantGagne() {
+		return montantGagne;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
