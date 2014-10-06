1bb05c5022a3f12f8844b15a0e7d99cb18368db2
narcatan <jlmanseau@live.ca>
2014-10-06 16:54:50 -0400

    adder les variables et méthodes.

--- Lab2_matricule/src/client/Client.java
+++ Lab2_matricule/src/client/Client.java
@@ -1,5 +1,101 @@
 package client;
+//Jean-Luc Manseau et Paul Champagne
 
 public class Client {
+private double argent;
+private String nom;
+private double distanceSouhaitee;
+private double distanceParcourue;
+private int jourNaissance;
+private int moisNaissance;
+private int anneeNaissance;
+enum classeConfortSouhaitee {TOURISTIQUE,AFFAIRE,LOCAL,PROMO,LUXE};
+classeConfortSouhaitee ClasseConfort;
+
+
+public double getArgent(){
+	return argent;
+}
+
+public double getAnneeNaissance(){
+	return anneeNaissance;
+}
+
+public classeConfortSouhaitee getClasseConfortSouhaitee(){
+	return ClasseConfort;
+}
+
+public double getDistanceParcourue(){
+	return distanceParcourue;
+}
+
+public double getDistanceSouhaitee(){
+	return distanceSouhaitee;
+}
+
+public void setDistancesouhaitee(double p_distanceSouhaitee){
+	distanceSouhaitee=p_distanceSouhaitee;
+}
+
+public double getJourNaissance(){
+	return jourNaissance;
+}
+
+public double getMoisNaissance(){
+	return moisNaissance;
+}
+
+public Client( int p_montantInitial, double p_distanceSouhaitee, String p_nom, classeConfortSouhaitee p_ClasseConfort){
+	argent=p_montantInitial;
+	distanceSouhaitee=p_distanceSouhaitee;
+	nom=p_nom;
+	p_ClasseConfort=ClasseConfort;
+	jourNaissance=24;
+	moisNaissance=12;
+	anneeNaissance=1996;
+}
+
+public void changeAnniversaire(int annee,int jour,int mois){
+	;
+}
+
+public boolean doitDebarquer(float distance){
+	boolean reponse=false;
+	if (getDistanceParcourue()== getDistanceSouhaitee()){
+		reponse=true;
+	}
+	return reponse;
+}
+
+
+public float payeMontant(float Montant){
+	
+}
+
+
+
+
+
+
+public String toString(){
+	return nom + "possede" + getArgent() + "a parcouru" + getDistanceParcourue() ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
+
