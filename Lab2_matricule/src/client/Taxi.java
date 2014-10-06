9ff08bf2a426ae912b04476f4e5b479721331e2a
pal705 <paulchampagne18@gmail.com>
2014-10-06 15:56:32 -0500

    creation des méthodes ainsi que L,initialisation du constructeur.

--- Lab2_matricule/src/client/Taxi.java
+++ Lab2_matricule/src/client/Taxi.java
@@ -11,8 +11,8 @@ package client;
 
 public class Taxi {
 
-	//Déclaration des constantes
-	
+	// Déclaration des constantes
+
 	double CONSOMMATION = 8.45;
 	double COUT_PAR_LITRE = 2.37;
 	double REVENU_PAR_KILOMETRE_AFFAIRE = 3;
@@ -21,61 +21,98 @@ public class Taxi {
 	double REVENU_PAR_KILOMETRE_PROMO = 2;
 	double REVENU_PAR_KILOMETRE_LUXE = 9.5;
 
-	//Déclaration des variables
-	
-	String nom;
-	double montantGagne;
-	double distanceParcourueTotale;
-	double distanceDepuisDepense;
-	int anneeCourante;
-	int moisCourant;
-	int jourCourant;
-	
-	
-	
-	
-	
+	// Déclaration des variables
+
+	private String nom;
+	private double montantGagne;
+	private double distanceParcourueTotale;
+	private double distanceDepuisDepense;
+	private int anneeCourante;
+	private int moisCourant;
+	private int jourCourant;
+	private Client place1;
+	private Client place2;
+
+	// Constructeur qui recois le nom en parametre
+
+	/**
+	 * 
+	 * @param p_nom
+	 *            : Recois le nom du Taxi et la transfert dans la variable nom.
+	 *            Initialise les variables à leurs valeurs par defaut et
+	 *            initialise les 2 places dans le taxi a null.
+	 */
+
 	public Taxi(String p_nom) {
-		
-		nom = p_nom;
-		
-				
+
+		this.nom = p_nom;
+		this.anneeCourante = 2014;
+		this.moisCourant = 12;
+		this.jourCourant = 24;
+		this.distanceParcourueTotale = 0;
+		this.distanceDepuisDepense = 0;
+		this.montantGagne = 0;
+		this.place1 = null;
+		this.place2 = null;
+
 	}
-
-
-
-
 
 	public double getMontantGagne() {
+
 		return montantGagne;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
 
+	/*
+	 * Reçoit un objet de type client représentant un client qui désire prendre
+	 * le taxi. Si le client est majeur et s’il a assez d’argent pour payer, le
+	 * taxi vérifie s’il lui reste une place disponible. Si la place1 est
+	 * disponible, le nouveau client y prend place (affectation de cet attribut
+	 * au nouveau client). Sinon si la seconde place est disponible, le nouveau
+	 * client y prend place. Évidemment si aucune place n’est disponible le
+	 * client ne peut pas embarquer.
+	 */
+
+	boolean embarque(Client celuiQuiEmbarque) {
+
+		boolean peutEmbarquer = false;
+
+		if (this.place1 == null) {
+			
+			peutEmbarquer = true;
+			this.place1 = celuiQuiEmbarque;
+		} else if (this.place2 == null) {
+			peutEmbarquer = true;
+			this.place2 = celuiQuiEmbarque;
+
+		}
+		return peutEmbarquer;
+	}
+
+	
+	
+	
+	
+	void appliqueDepense() {
+
+	}
+
+	void changeDateCourante(int p_JourCourant, int p_MoisCourante,
+			int p_AnneeCourante) {
+
+	}
+
+	void roule(int kilometre) {
+
+	}
+
+	boolean estMajeur(Client celuiQuiEmbarque) {
+
+		return false;
+	}
+
+	boolean peutPayer(Client celuiQuiEmbarque) {
+
+		return false;
+	}
+
+}
